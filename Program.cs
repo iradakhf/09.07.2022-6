bfd40a714cb83ac39f4cd945e179a2e39e79d1b5
iradakhf <iradakhf@code.edu.az>
2022-07-09 08:18:27 +0400

    add ConsoleApp14

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+namespace Method
+{
+    class Program
+    {
+        public static void Main (string[] args)
+        {
+            findTheNearestSquareRoot(500);
+        }
+        public static void findTheNearestSquareRoot(double number)
+        {
+            if (number % Math.Sqrt(number) == 0)
+            {
+                Console.WriteLine(Math.Sqrt(number)); 
+            }
+            else
+            {
+                Console.WriteLine(Math.Truncate(Math.Sqrt(number))); 
+            }
+           
+            
+        }
+    }
+}
